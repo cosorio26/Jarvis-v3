f5610beada74f5634f4969e73104fe5c572292b3
Carlos Alberto Osorio Jaramillo <ElCoso@MacBook-Air-de-Juan.local>
2025-08-02 16:06:05 -0500

    Limpieza de codigo en telegram client

--- services/telegram_client.py
+++ services/telegram_client.py
@@ -16,20 +16,7 @@ def send_message_to_telegram(chat_id, message):
 
     # Enviamos el mensaje a Telegram
     response = requests.post(url, json=params)
-
-    # Imprimimos la respuesta del servidor
-    #print(f"Respuesta del servidor: {message}")
-    #botToken = os.getenv("TELEGRAM_BOT_TOKEN")
-    #print(f"Bot Token: {botToken}")
-
-    #send_text = f"https://api.telegram.org/bot{botToken}/sendMessage?chat_id=" + str(chat_id) + \
-    #           "&parse_mode=HTML&text=" + message
     
-    # Imprimimos lo que se va a enviar
-    #print(f"Enviando mensaje a Telegram: {send_text}")
-    # Realizamos la solicitud GET para enviar el mensaje
-    #response = requests.get(send_text)
-
     # Verificamos si la solicitud fue exitosa
     if response.status_code != 200:
         raise Exception(f"Error al enviar mensaje a Telegram: {response.text}")
