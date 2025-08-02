75ac18255ae1b42805713f5776a3fbdfe72500b6
Carlos Alberto Osorio Jaramillo <ElCoso@MacBook-Air-de-Juan.local>
2025-08-02 15:39:26 -0500

    Cambio de variable de entorno Token Bot

--- services/telegram_client.py
+++ services/telegram_client.py
@@ -20,7 +20,7 @@ def send_message_to_telegram(chat_id, message):
     # Imprimimos la respuesta del servidor
     #print(f"Respuesta del servidor: {message}")
 
-    send_text = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_BOT_TOKEN')}/sendMessage?chat_id=" + str(chat_id) + \
+    send_text = f"https://api.telegram.org/bot{os.getenv("TELEGRAM_BOT_TOKEN")}/sendMessage?chat_id=" + str(chat_id) + \
                 "&parse_mode=HTML&text=" + message
     
     # Imprimimos lo que se va a enviar
