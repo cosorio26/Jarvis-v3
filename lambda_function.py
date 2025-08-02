bdc64f374b0c91e38002c5aea8f72f6107172560
Carlos Alberto Osorio Jaramillo <ElCoso@MacBook-Air-de-Juan.local>
2025-08-02 00:32:20 -0500

    Se agrega un control de cuota para peticiones contra bedrock

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
 # Importamos las librerias necesarias
 import json
+import time
+import random
 from handlers.telegram_handler import extract_message
 from services.bedrock_client import invoke_bedrock
 from services.telegram_client import send_message_to_telegram
@@ -14,8 +16,15 @@ def lambda_handler(event, context):
         chat_id, message_text = extract_message(event)
 
         # Invocamos a Bedrock con el mensaje recibido
-        response = invoke_bedrock(message_text)
-
+        try:
+            response = invoke_bedrock(message_text)
+        except ThrottlingException:
+            delay = random.uniform(1.5, 3.0) # Esperamos un tiempo aleatorio entre 1.5 y 3 segundos
+            print(f"Throttling detectado, esperando {delay} segundos antes de reintentar...")
+            time.sleep(delay)
+            # Reintentamos invocar a Bedrock
+            response = invoke_bedrock(message_text)
+                                      
         # Enviamos la respuesta a Telegram
         send_message_to_telegram(chat_id, response)
 
